0793b04964e2fcf1f1e72656ebb72f74f1c4ce9b
hiroomi ohori <tkgyb713@yahoo.co.jp>
2020-05-08 00:50:59 +0900

    update network logger

--- app/src/main/java/com/example/demo/logsample/MainActivity.java
+++ app/src/main/java/com/example/demo/logsample/MainActivity.java
@@ -1,30 +1,21 @@
 package com.example.demo.logsample;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.ArrayMap;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import com.example.demo.logsample.log.Log;
 import com.example.demo.logsample.log.LogRepository;
 import com.example.demo.logsample.log.Type;
-import com.google.gson.Gson;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiConsumer;
 
 public class MainActivity extends AppCompatActivity {
     private Map<String,Long> usageSeconds = new HashMap<>();
-    private MutableLiveData<Long> ttlSeconds = new MutableLiveData<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,43 +37,62 @@ public class MainActivity extends AppCompatActivity {
         });
         initObservables();
     }
+
     private void initObservables() {
         LogRepository repository = LogRepository.getInstance();
         repository.init(getApplication());
-
-        ttlSeconds.observe(this,v -> {
-            Long ttl = usageSeconds.values().stream().mapToLong(Long::longValue).sum();
-            TextView textView = findViewById(R.id.ttlSeconds);
-            textView.setText("Total:" + ttl + "sec");
-        });
 
         repository.observeStats(LocalDate.now(), Type.ACTIVE).observe(this, seconds -> {
             if(seconds == null) return;
             TextView textView = findViewById(R.id.activeSeconds);
             textView.setText("Active:" + seconds + "sec");
             usageSeconds.put(Type.ACTIVE,seconds);
-            ttlSeconds.postValue(0L);
         });
         repository.observeStats(LocalDate.now(), Type.MOVE_ACTIVE).observe(this, seconds -> {
             if(seconds == null) return;
             TextView textView = findViewById(R.id.moveActiveSeconds);
             textView.setText("Move Active:" + seconds + "sec");
             usageSeconds.put(Type.MOVE_ACTIVE,seconds);
-            ttlSeconds.postValue(0L);
         });
         repository.observeStats(LocalDate.now(), Type.MOVE_INACTIVE).observe(this, seconds -> {
             if(seconds == null) return;
             TextView textView = findViewById(R.id.moveInactiveSeconds);
             textView.setText("Move Inactive:" + seconds + "sec");
             usageSeconds.put(Type.MOVE_INACTIVE,seconds);
-            ttlSeconds.postValue(0L);
         });
         repository.observeStats(LocalDate.now(), Type.INACTIVE).observe(this, seconds -> {
             if(seconds == null) return;
             TextView textView = findViewById(R.id.inactiveSeconds);
             textView.setText("Inactive:" + seconds + "sec");
             usageSeconds.put(Type.INACTIVE,seconds);
-            ttlSeconds.postValue(0L);
         });
+        repository.observeStats(LocalDate.now(), Type.WIFI_GOOD).observe(this, seconds -> {
+            if(seconds == null) return;
+            TextView textView = findViewById(R.id.wifiGood);
+            textView.setText("wifiGood:" + seconds + "sec");
+            usageSeconds.put(Type.WIFI_GOOD,seconds);
+        });
+
+        repository.observeStats(LocalDate.now(), Type.WIFI_MID).observe(this, seconds -> {
+            if(seconds == null) return;
+            TextView textView = findViewById(R.id.wifiMid);
+            textView.setText("wifiMid:" + seconds + "sec");
+            usageSeconds.put(Type.WIFI_MID,seconds);
+        });
+
+        repository.observeStats(LocalDate.now(), Type.WIFI_LOW).observe(this, seconds -> {
+            if(seconds == null) return;
+            TextView textView = findViewById(R.id.wifiLow);
+            textView.setText("wifiLow:" + seconds + "sec");
+            usageSeconds.put(Type.WIFI_LOW,seconds);
+        });
+
+        repository.observeStats(LocalDate.now(), Type.WIFI_LOST).observe(this, seconds -> {
+            if(seconds == null) return;
+            TextView textView = findViewById(R.id.wifiLost);
+            textView.setText("wifiLost:" + seconds + "sec");
+            usageSeconds.put(Type.WIFI_LOST,seconds);
+        });
+
     }
 }
--- app/src/main/java/com/example/demo/logsample/UsageTimeLogger.java
+++ app/src/main/java/com/example/demo/logsample/UsageTimeLogger.java
@@ -8,9 +8,16 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
 import android.os.PowerManager;
+
+import androidx.annotation.NonNull;
 
 import com.example.demo.logsample.log.LogRepository;
 import com.example.demo.logsample.log.Type;
@@ -18,20 +25,26 @@ import com.example.demo.logsample.log.Type;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-public final class UsageTimeLogger extends BroadcastReceiver implements SensorEventListener {
+public final class UsageTimeLogger implements SensorEventListener {
     private final Context context;
     private final Handler handler = new Handler();
     private Long lastStateChangeSecond;
+    private Long lastNetworkChangeSecond;
     private PowerManager powerManager;
     private SensorManager sensorManager;
+    private WifiManager wifiManager;
+    private ConnectivityManager connectivityManager;
     private boolean isActive;
     private boolean isMoving;
-    private String nowState;
+    private String powerState;
+    private String networkState;
 
     public UsageTimeLogger(Context context) {
         this.context = context;
         powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
         sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
     }
     public void start(){
         IntentFilter filter = new IntentFilter();
@@ -42,37 +55,23 @@ public final class UsageTimeLogger extends BroadcastReceiver implements SensorEv
         filter.addAction(Intent.ACTION_BATTERY_LOW);
         filter.addAction(Intent.ACTION_SCREEN_ON);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
-        filter.addAction(WifiManager.RSSI_CHANGED_ACTION);
-        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
-        context.registerReceiver(this,filter);
+        context.registerReceiver(powerStateMonitor,filter);
         Sensor stepDetectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
         sensorManager.registerListener(this, stepDetectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
         isActive = powerManager.isInteractive();
         isMoving = false;
         lastStateChangeSecond = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
-        nowState = Type.ACTIVE;
         updateActiveState();
+        connectivityManager.registerDefaultNetworkCallback(networkCallback);
+        updateNetworkState();
     }
     public void stop(){
         updateActiveState();
+        updateNetworkState();
         sensorManager.unregisterListener(this);
-        context.unregisterReceiver(this);
-    }
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        switch (intent.getAction()) {
-            case Intent.ACTION_SCREEN_ON:
-                isActive = true;
-                updateActiveState();
-                break;
-            case Intent.ACTION_SCREEN_OFF:
-                isActive = false;
-                updateActiveState();
-                break;
-            case Intent.ACTION_BATTERY_CHANGED:
-                break;
-        }
+        context.unregisterReceiver(powerStateMonitor);
+        context.unregisterReceiver(wifiSignalMonitor);
+        connectivityManager.unregisterNetworkCallback(networkCallback);
     }
 
     @Override
@@ -92,12 +91,83 @@ public final class UsageTimeLogger extends BroadcastReceiver implements SensorEv
 
     private void updateActiveState() {
         Long now = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
-        LogRepository.getInstance().insertStats(nowState,now - lastStateChangeSecond);
+        if(powerState != null) {
+            LogRepository.getInstance().insertStats(powerState, now - lastStateChangeSecond);
+        }
         if(isActive) {
-            nowState = isMoving ? Type.MOVE_ACTIVE : Type.ACTIVE;
+            powerState = isMoving ? Type.MOVE_ACTIVE : Type.ACTIVE;
         } else {
-            nowState = isMoving ? Type.MOVE_INACTIVE : Type.INACTIVE;
+            powerState = isMoving ? Type.MOVE_INACTIVE : Type.INACTIVE;
         }
         lastStateChangeSecond = now;
     }
+    private BroadcastReceiver powerStateMonitor = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()) {
+                case Intent.ACTION_SCREEN_ON:
+                    isActive = true;
+                    updateActiveState();
+                    break;
+                case Intent.ACTION_SCREEN_OFF:
+                    isActive = false;
+                    updateActiveState();
+                    break;
+                case Intent.ACTION_BATTERY_CHANGED:
+                    break;
+            }
+        }
+    };
+    private void updateNetworkState() {
+        Long now = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+        if(networkState != null){
+            LogRepository.getInstance().insertStats(networkState, now - lastNetworkChangeSecond);
+        }
+        WifiInfo info = wifiManager.getConnectionInfo();
+        if(info.getSupplicantState().equals(SupplicantState.DISCONNECTED)) {
+            networkState = Type.WIFI_LOST;
+        } else {
+            int level = WifiManager.calculateSignalLevel(info.getRssi(), 3);
+            switch (level) {
+                case 1:
+                    networkState = Type.WIFI_MID;
+                    break;
+                case 2:
+                    networkState = Type.WIFI_GOOD;
+                    break;
+                default:
+                    networkState = Type.WIFI_LOW;
+                    break;
+            }
+        }
+        lastNetworkChangeSecond = now;
+    }
+    private BroadcastReceiver wifiSignalMonitor = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateNetworkState();
+        }
+    };
+    private ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
+        @Override
+        public void onAvailable(@NonNull Network network) {
+            super.onAvailable(network);
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(WifiManager.RSSI_CHANGED_ACTION);
+            context.registerReceiver(wifiSignalMonitor,filter);
+            updateNetworkState();
+        }
+        @Override
+        public void onLost(@NonNull Network network) {
+            super.onLost(network);
+            context.unregisterReceiver(wifiSignalMonitor);
+            updateNetworkState();
+        }
+        @Override
+        public void onUnavailable() {
+            super.onUnavailable();
+            context.unregisterReceiver(wifiSignalMonitor);
+            updateNetworkState();
+        }
+    };
 }
